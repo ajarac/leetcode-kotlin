6f691b280566ab50988273c5ab1dcf1f26e0474f
Angel Jara <ajarac92@gmail.com>
2022-05-24 19:23:04 +0400

    424

--- src/main/kotlin/Solution424.kt
+++ src/main/kotlin/Solution424.kt
@@ -2,7 +2,20 @@ package main
 
 class Solution424 {
     fun characterReplacement(s: String, k: Int): Int {
+        val map = mutableMapOf<Char, Int>()
+        var l = 0
+        var res = 0
+        var max = 0
 
-        return 0
+        for (r in s.indices) {
+            map[s[r]] = map.getOrDefault(s[r], 0) + 1
+            max = maxOf(max, map[s[r]]!!)
+            while (r - l + 1 - max > k) {
+                map[s[l]] = map[s[l]]!! - 1
+                l++
+            }
+            res = maxOf(res, r - l + 1)
+        }
+        return res
     }
 }
--- src/test/kotlin/Solution424Test.kt
+++ src/test/kotlin/Solution424Test.kt
@@ -6,8 +6,9 @@ internal class Solution424Test {
     @Test
     fun test() {
         val solution = Solution424()
-
+        assertEquals(3, solution.characterReplacement("BAAA", 0))
         assertEquals(4, solution.characterReplacement("ABAB", 2))
         assertEquals(4, solution.characterReplacement("AABABBA", 1))
+
     }
 }
